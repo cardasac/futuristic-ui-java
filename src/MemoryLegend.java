659f2a202cd6283a119fe796ae95eb4e29bf1ad8
Alexandru Cardas <c17504869@mydit.ie>
2019-04-24 13:55:22 +0100

    Add constructor to MemoryLegend class

--- src/MemoryLegend.java
+++ src/MemoryLegend.java
@@ -1,12 +1,37 @@
 public class MemoryLegend
 {
 	private UI ui;
+	private float coordinateX;
+	private float coordinateY;
+	private float rotation;
+	private float colourMemory;
+
+	private float rectHeight;
+	private float rectWidth;
+	private float circleSize;
 
 
-	MemoryLegend(UI ui)
+	MemoryLegend(UI ui, float coordinateX, float coordinateY, float rotation, float colourMemory)
 	{
 		this.ui = ui;
+		this.coordinateX = coordinateX;
+		this.coordinateY = coordinateY;
+		this.rotation = rotation;
+		this.colourMemory = colourMemory;
 	}
 
+	void render()
+	{
+		ui.fill(colourMemory);
+		rectHeight = 60;
+		rectWidth = 20;
+		ui.circle();
+		ui.rect(coordinateX, coordinateY, rectHeight, rectWidth);
+	}
+
+	void update()
+	{
+
+	}
 
 }
