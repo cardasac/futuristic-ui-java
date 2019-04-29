9360451facdb2cef9cfdfef5feec6ea006d50a7a
Alexandru Cardas <c17504869@mydit.ie>
2019-04-29 11:41:33 +0100

    Create static class to reduce amount of code on main

--- src/Menu_Options.java
+++ src/Menu_Options.java
@@ -21,4 +21,24 @@ public class Menu_Options
 	{
 		return name + "\t" + description;
 	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
 }
